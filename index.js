5ea3fdaedc1982aaca1a9b81358aa0e1e83dd748
Aria Balaei <ariabalayi@gmail.com>
2023-08-19 15:40:50 +0200

    scaleLinear and scaleBand added

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ const svg = canvas.append('svg')
 //add rect
 const rect = svg.selectAll('rect');
     d3.json('test.json').then(data => {
-      console.log(data)
+
+      //scaleLinear
+      const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.height)])
+        .range([0, 250]);
+      
+      //scaleBand 
+      const x = d3.scaleBand()
+                  .domain(data.map(item => item.fill))
+                  .range([0, 500])
+                  .paddingInner(0.09);
+
+  
       rect.data(data)
           .enter().append('rect')
-          .attr('width', (d, i) => d.width)
-          .attr('height', (d, i) => d.height)
+          .attr('width', (x.bandwidth))
+          .attr('height', (d, i) => y(d.height))
           .attr('fill' , (d) => d.fill)
-          .attr('x', (d ,i) => i*35)
+          .attr('x', (d ,i) => x(d.fill))
     });
